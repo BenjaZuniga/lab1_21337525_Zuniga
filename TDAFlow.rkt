4fec00aae7ad18d5daef3d80aa86016ea2ceb8f8
BenjaAZ <115362990+Benja1407@users.noreply.github.com>
2023-09-12 18:30:44 -0300

    Update TDAFlow.rkt

--- TDAFlow.rkt
+++ TDAFlow.rkt
@@ -4,12 +4,35 @@
 #|Flow|#
 
 #|Constructor|#
-(define (flow name . Option)(list name Option))
-#|Modificadores|#
-(define(flow-add-option flow option)(append(cadr(flow))option))
-
+(define (flow name . Option)
+  (if(flow? (list name Option))
+            (if(integer? (option-uniques-id (list name Option)))
+               "IDs de opciones repetidos"
+               (list name Option))
+            "Los parametros ingresados no son validos"))
+#|Pertenecia|#
+(define (flow? flow)
+  (if(string? (SelectName flow))
+     (if(or (map option?(SelectOption flow))(map empty? (SelectOption flow)))
+        #t #f)#f))
 #|Selectores|#
-;SelectName
+
+(define(SelectId flow)(car flow))
+
 (define (SelectName flow)(car flow))
-;SelectOption
+
 (define (SelectOption flow)(cadr flow))
+
+#|Modificadores|#
+(define(flow-add-option fl option)
+  (if(integer?
+      (option-uniques-id(list (SelectName fl)
+                              (append(SelectOption fl) (list option)))))
+     "IDs de opciones repetidos"
+     (list (SelectName fl) (append(SelectOption fl) (list option)))))
+
+#|Otros|#
+(define (option-uniques-id flow)
+  (check-duplicates(append(list )
+                          (map select-code(SelectOption flow)))
+                   #:default #t))
