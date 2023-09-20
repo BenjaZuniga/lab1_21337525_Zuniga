c412a093e6925da5e678899df9ec9db4f87b1e8a
Benjamín Zúñiga <115362990+Benja1407@users.noreply.github.com>
2023-09-20 10:16:32 -0300

    Update TDAChatbot.rkt

--- TDAChatbot.rkt
+++ TDAChatbot.rkt
@@ -4,12 +4,22 @@
 (provide (all-defined-out))
 
 #|Constructor|#
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
 (define (chatbot id name welcomeMessage . flows)
   (if(chatbot? (list id name welcomeMessage flows))
      (if(integer? (flow-uniques-id(list id name welcomeMessage flows)))
                   "IDs de flow repetidos"
                   (list id name welcomeMessage flows))#f))
 #|Pertenencia|#
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
 (define(chatbot? chatbot)
   (if(integer? (select-chatbot-id chatbot))
      (if(string? (select-chatbot-name chatbot))
@@ -19,6 +29,11 @@
                  )#t #f)#f)#f)#f))
 
 #|Modificadores|#
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
 (define (chatbot-add-flow cb flow)
   (define (aux cb flow new-chatbot)
                (if(list? cb)
@@ -37,15 +52,40 @@
   (aux cb flow cb))
 
 #|Selectores|#
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
 (define(select-chatbot-id chatbot)(car chatbot))
 
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
 (define (select-chatbot-name chatbot)(cadr chatbot))
 
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
 (define(select-chatbot-wM chatbot)(caddr chatbot))
 
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
 (define(select-chatbot-flow chatbot)(cadddr chatbot))
 
 #|Otros|#
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
 (define (flow-uniques-id chatbot)
   (check-duplicates(append(list )
                           (map select-id(select-chatbot-flow chatbot)))
