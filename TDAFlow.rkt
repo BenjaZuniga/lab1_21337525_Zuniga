8d013ca2cb086ce02d27fab0f361e6e97e05528d
Benjamín Zúñiga <115362990+Benja1407@users.noreply.github.com>
2023-09-27 23:04:49 -0300

    Update TDAFlow.rkt

--- TDAFlow.rkt
+++ TDAFlow.rkt
@@ -1,7 +1,4 @@
-#lang racket
-(require "TDAOption.rkt")
-(provide (all-defined-out))
-#|TDA Flow|#
+#|..........................TDA Flow..........................|#
 
 #|..........................Constructor..........................|#
 ;Nombre de la función:
@@ -9,12 +6,11 @@
 ;Recorrido:
 ;Tipo de recursion:
 ;Descripción de la función:
-(define (flow name . Option)
-  (if(flow? (list name Option))
-            (if(integer? (option-uniques-id (list name Option)))
-               "IDs de opciones repetidos"
-               (list name Option))
-            "Los parametros ingresados no son validos"))
+(define (flow id name . Option)
+  (list id name (if(not(null? Option))
+                   (remove (list)(new-Option Option))
+                   Option)))
+
 
 #|..........................Pertenecia..........................|#
 ;Nombre de la función:
@@ -23,30 +19,32 @@
 ;Tipo de recursion:
 ;Descripción de la función:
 (define (flow? flow)
-  (if(string? (SelectName flow))
-     (if(or (map option?(SelectOption flow))(map empty? (SelectOption flow)))
-        #t #f)#f))
+  (if(integer? (select-flow-id flow))
+     (if(string? (select-flow-name flow))
+        (if(or (map option?(select-flow-Option flow))
+               (null? (select-flow-Option flow)))
+        #t #f)#f)#f))
 #|..........................Selectores..........................|#
 ;Nombre de la función:
 ;Dominio:
 ;Recorrido:
 ;Tipo de recursion:
 ;Descripción de la función:
-(define(SelectId flow)(car flow))
+(define(select-flow-id flow)(car flow))
 
 ;Nombre de la función:
 ;Dominio:
 ;Recorrido:
 ;Tipo de recursion:
 ;Descripción de la función:
-(define (SelectName flow)(car flow))
+(define (select-flow-name flow)(cadr flow))
 
 ;Nombre de la función:
 ;Dominio:
 ;Recorrido:
 ;Tipo de recursion:
 ;Descripción de la función:
-(define (SelectOption flow)(cadr flow))
+(define (select-flow-Option flow)(caddr flow))
 
 #|..........................Modificadores..........................|#
 ;Nombre de la función:
@@ -54,12 +52,12 @@
 ;Recorrido:
 ;Tipo de recursion:
 ;Descripción de la función:
-(define(flow-add-option fl option)
-  (if(integer?
-      (option-uniques-id(list (SelectName fl)
-                              (append(SelectOption fl) (list option)))))
-     "IDs de opciones repetidos"
-     (list (SelectName fl) (append(SelectOption fl) (list option)))))
+(define(flow-add-option flow option)
+ (if(boolean?(member (select-option-code option)
+                     (get-option-ids (select-flow-Option flow))))
+    (list (select-flow-id flow)(select-flow-name flow)
+          (append (select-flow-Option flow)(list option)))
+    flow))
 
 #|..........................Otros..........................|#
 ;Nombre de la función:
@@ -67,7 +65,23 @@
 ;Recorrido:
 ;Tipo de recursion:
 ;Descripción de la función:
-(define (option-uniques-id flow)
-  (check-duplicates(append(list )
-                          (map select-code(SelectOption flow)))
-                   #:default #t))
+(define new-Option(lambda(option)
+  (define create-Option(lambda(option ids)
+    (if(not (null? (cdr option)))
+       (if (list?(member (select-option-code (car option)) ids))
+        (cons(car option)(create-Option(cdr option)(remove (select-option-code (car option)) ids)))
+        (create-Option(cdr option) ids ))
+       (if (list?(member (select-option-code (car option)) ids))
+        (cons(car option) null)
+        (cons null null )))))
+                    (create-Option option (get-option-ids option))))
+
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
+(define (get-option-ids option)
+  (if(not(null? option))
+     (remove-duplicates (map select-option-code option))
+     (list)))
