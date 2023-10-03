ac5c5717ca635d149270e8cbe40f481e86207f9d
Benjamín Zúñiga <115362990+Benja1407@users.noreply.github.com>
2023-10-03 03:07:47 -0300

    Update TDAUser.rkt

--- TDAUser.rkt
+++ TDAUser.rkt
@@ -1,9 +1,32 @@
 #lang racket
 (provide(all-defined-out))
 
+#|..........................Constructor..........................|#
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
 (define user (list (list)(list)))
 
-
+#|..........................Selectores..........................|#
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
 (define(select-user-register user )(car user))
 
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
 (define(select-user-login user)(cadr user))
+
+;Nombre de la función:
+;Dominio:
+;Recorrido:
+;Tipo de recursion:
+;Descripción de la función:
+(define(get-user-login user)(caadr user))
